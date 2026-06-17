71d53dd2249144407d5f9fbbd96a8967f15ab02b
kzitelli-art <kzitelli@gmail.com>
2026-06-17 09:43:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ const moneyBoardModule = require('./moneyboard');
 const attributionsModule = require('./attributions');
 const portfolioModule = require('./portfolio');
 const snapshotLoaderModule = require('./snapshot_loader');
+const seedEndpointModule = require('./seed_endpoint');
 const plaidModule = require('./plaid'); // Plaid: second feed into bank_transactions (031); fail-soft when unconfigured
 const compareModule = require("./compare");   // report comparison layer (the hook)
 const explainModule = require("./explain");
@@ -3104,6 +3105,7 @@ app.use('/', moneyBoardModule({ pool }));
 app.use('/', attributionsModule({ pool }));
 app.use('/api', portfolioModule({ pool }));
 app.use('/', snapshotLoaderModule({ pool, upload }));
+app.use('/', seedEndpointModule({ pool }));
 app.use('/', plaidModule({ pool }));
 app.use("/", exposureModule({ pool }));
 app.use("/", reportingModule({ pool }));
